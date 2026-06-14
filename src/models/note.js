1dab9c7c467a6d7b2c6bbaa5fc906d32ce0c001d
Anastasia Baranenko <anastasiyabaranenko78@gmail.com>
2026-06-14 18:43:38 +0300

    add new in 4

--- src/models/note.js
+++ src/models/note.js
@@ -32,6 +32,6 @@ const noteSchema = new Schema(
   },
 );
 
-noteSchema.index({ userId: 1, title: 'text', content: 'text' });
+noteSchema.index({ tag: 1, userId: 1, title: 'text', content: 'text' });
 
 export const Note = model('Note', noteSchema);
